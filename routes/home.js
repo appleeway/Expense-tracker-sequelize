2a1a68825f669cd1f3b814398866ecbbfd3021e7
Appleeway <applee62002@gmail.com>
2020-04-17 14:34:06 +0800

    Add month and category filter

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 // routes/home.js
 const express = require('express')
 const router = express.Router()
+const { Op } = require('sequelize')
 
 //載入 database
 const db = require('../models')
@@ -11,13 +12,26 @@ const Record = db.Record
 const { authenticated } = require('../config/auth')
 
 router.get('/', authenticated, (req, res) => {
+  const months = ['-01-', '-02-', '-03-', '-04-', '-05-', '-06-', '-07-', '-08-', '-09-', '-10-', '-11-', '-12-']
+  const month = req.query.month || ''
+  const category = req.query.category || ''
+  const startDate = `2020-${month}-01`
+  const endDate = `2020-${month + 1}-01`
+
+  console.log(month)
+  console.log(category)
+
   User.findByPk(req.user.id)
     .then(user => {
       if (!user) throw new Error("user not found")
       return Record.findAll({
         raw: true,
         nest: true,
-        where: { UserId: req.user.id }
+        where: {
+          UserId: req.user.id,
+          category: { [Op.substring]: category },
+          date: { [Op.substring]: month }
+        }
       })
     })
     .then((records) => {
@@ -25,7 +39,7 @@ router.get('/', authenticated, (req, res) => {
       for (i = 0; i < records.length; i++) {
         totalAmount += records[i].amount
       }
-      return res.render('index', { records: records, totalAmount })
+      return res.render('index', { records, totalAmount, category, months, month })
     })
     .catch((error) => { return res.status(422).json(error) })
 
