c1598ae3ad95a1b1cb64859e44be047c00aaa808
Appleeway <applee62002@gmail.com>
2020-04-27 18:07:57 +0800

    Modify category filter in index page

--- routes/home.js
+++ routes/home.js
@@ -14,8 +14,7 @@ const { authenticated } = require('../config/auth')
 router.get('/', authenticated, (req, res) => {
   const months = ['-01-', '-02-', '-03-', '-04-', '-05-', '-06-', '-07-', '-08-', '-09-', '-10-', '-11-', '-12-']
   const month = req.query.month || ''
-
-  // const category = req.query.category || ''
+  const CategoryId = req.query.CategoryId || ''
 
   User.findByPk(req.user.id)
     .then(user => {
@@ -25,7 +24,7 @@ router.get('/', authenticated, (req, res) => {
         nest: true,
         where: {
           UserId: req.user.id,
-          // category: { [Op.substring]: category },
+          CategoryId: { [Op.substring]: CategoryId },
           date: { [Op.substring]: month }
         }
       })
@@ -35,7 +34,7 @@ router.get('/', authenticated, (req, res) => {
       for (i = 0; i < records.length; i++) {
         totalAmount += records[i].amount
       }
-      return res.render('index', { records, totalAmount, months, month })
+      return res.render('index', { records, totalAmount, months, month, CategoryId })
     })
     .catch((error) => { return res.status(422).json(error) })
 
